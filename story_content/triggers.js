520d0f907c6b45931f76853194d67a0587f6c2ba
HRP24 <hemantpawar2688@gmail.com>
2025-06-08 20:34:42 +0530

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,47 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5qwB2FhvuLK":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('5wiIhmZ4Zf0');
+const duration = 750;
+const easing = 'ease-out';
+const id = '62z2Ie9jlnY';
+const teeterAmount = 4;
+player.addForTriggers(
+id,
+target.animate(
+[ {rotate: '0deg' }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: `${teeterAmount}deg` }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: '0deg' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
